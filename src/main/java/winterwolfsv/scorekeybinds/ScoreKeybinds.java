39ed78f885f7dbb20d7d8a4d11daa2ad25dea2f7
WhiteWolfSV <what112222@gmail.com>
2023-02-26 23:30:48 +0100

    Initial commit

---
+++ src/main/java/winterwolfsv/scorekeybinds/ScoreKeybinds.java
@@ -0,0 +1,10 @@
+package winterwolfsv.scorekeybinds;
+
+import net.fabricmc.api.ModInitializer;
+
+public class ScoreKeybinds implements ModInitializer {
+    @Override
+    public void onInitialize() {
+
+    }
+}
