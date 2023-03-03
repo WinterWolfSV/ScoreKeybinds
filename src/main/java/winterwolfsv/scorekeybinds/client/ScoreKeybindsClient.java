6a6108783da924246488819e1356157c1cab499e
WinterWolfSV <what112222@gmail.com>
2023-03-03 22:02:06 +0100

    Bug fixes

--- src/main/java/winterwolfsv/scorekeybinds/client/ScoreKeybindsClient.java
+++ src/main/java/winterwolfsv/scorekeybinds/client/ScoreKeybindsClient.java
@@ -1,8 +1,6 @@
 package winterwolfsv.scorekeybinds.client;
 
 import net.fabricmc.api.ClientModInitializer;
-import net.fabricmc.api.EnvType;
-import net.fabricmc.api.Environment;
 import net.fabricmc.fabric.api.client.event.lifecycle.v1.ClientTickEvents;
 import net.fabricmc.fabric.api.client.keybinding.v1.KeyBindingHelper;
 import net.fabricmc.loader.api.FabricLoader;
@@ -17,13 +15,16 @@ import java.nio.file.Files;
 import java.nio.file.Paths;
 import java.util.Objects;
 import java.util.Scanner;
+import java.util.concurrent.atomic.AtomicBoolean;
 
-@Environment(EnvType.CLIENT)
+
 public class ScoreKeybindsClient implements ClientModInitializer {
 
     private JSONArray getConfigData() {
         String path = FabricLoader.getInstance().getConfigDir() + "\\scorekeybinds.json";
+        System.out.println(path);
         if (new File(path).exists()) {
+            System.out.println("Path to config file for mod exists.");
             try {
                 InputStream inputStream = Files.newInputStream(Paths.get(path));
                 Scanner scanner = new Scanner(inputStream, "UTF-8");
@@ -43,20 +44,24 @@ public class ScoreKeybindsClient implements ClientModInitializer {
     private void createKeybinds() {
         try {
             int i = 0;
-            for (Object obj : Objects.requireNonNull(getConfigData())) {
-                if (obj instanceof JSONObject) {
-                    JSONObject jsonObject = (JSONObject) obj;
-                    String stringKey = jsonObject.getString("key").toLowerCase();
-                    int key = -1;
-                    if (!stringKey.isEmpty() && stringKey.charAt(0) != ' ') {
-                        key = stringKey.charAt(0) - 32;
+            if (getConfigData() != null) {
+                for (Object obj : getConfigData()) {
+                    if (obj instanceof JSONObject) {
+                        JSONObject jsonObject = (JSONObject) obj;
+                        String stringKey = jsonObject.getString("default_key").toLowerCase();
+                        int key = -1;
+                        if (!stringKey.isEmpty() && stringKey.charAt(0) != ' ') {
+                            key = stringKey.charAt(0) - 32;
+                        }
+                        String command = jsonObject.getString("command");
+                        String name = jsonObject.getString("name");
+                        System.out.println("Creating keybind: '" + name + "' With keybind: '" + stringKey + "' For command: '" + command + "'");
+                        keyBindings[i] = KeyBindingHelper.registerKeyBinding(new KeyBinding(name, InputUtil.Type.KEYSYM, key, "Score Keybindings"));
                     }
-                    String command = jsonObject.getString("command");
-                    String name = jsonObject.getString("name");
-                    System.out.println("Creating keybind: '" + name + "' With keybind: '" + stringKey + "' For command: '" + command + "'");
-                    keyBindings[i] = KeyBindingHelper.registerKeyBinding(new KeyBinding(name, InputUtil.Type.KEYSYM, key, "Score Keybindings"));
+                    i++;
                 }
-                i++;
+            } else {
+                System.out.println("Config file not found for mod: ScoreKeybinds. Disabling mod.");
             }
         } catch (Exception e) {
             System.out.println(e);
@@ -65,23 +70,37 @@ public class ScoreKeybindsClient implements ClientModInitializer {
 
     @Override
     public void onInitializeClient() {
+        AtomicBoolean disableMod = new AtomicBoolean(false);
         try {
-            createKeybinds();
-            ClientTickEvents.END_CLIENT_TICK.register(client -> {
-                for (int i = 0; i < keyBindings.length; i++) {
-                    if (keyBindings[i].wasPressed()) {
-                        assert client.player != null;
-                        if (Objects.requireNonNull(getConfigData()).getJSONObject(i).getString("command").charAt(0) == '/') {
-                            client.player.sendChatMessage(Objects.requireNonNull(getConfigData()).getJSONObject(i).getString("command"));
-                        } else {
-                            client.player.sendChatMessage("/" + Objects.requireNonNull(getConfigData()).getJSONObject(i).getString("command"));
+            if (getConfigData() != null && !disableMod.get()) {
+                createKeybinds();
+                ClientTickEvents.END_CLIENT_TICK.register(client -> {
+                    for (int i = 0; i < keyBindings.length; i++) {
+                        try {
+                            if (keyBindings[i].wasPressed()) {
+                                if (getConfigData().getJSONObject(i).getString("command").charAt(0) == '/') {
+                                    client.player.sendChatMessage(Objects.requireNonNull(getConfigData()).getJSONObject(i).getString("command"));
+                                } else {
+                                    client.player.sendChatMessage("/" + Objects.requireNonNull(getConfigData()).getJSONObject(i).getString("command"));
+                                }
+                                break;
+                            }
+                        } catch (Exception e) {
+                            System.out.println(e);
+                            disableMod.set(true);
                         }
-                        break;
+
                     }
-                }
-            });
+                });
+            } else {
+                System.out.println("Config file not found for mod: ScoreKeybinds. Disabling mod.");
+                disableMod.set(true);
+
+            }
+
         } catch (Exception e) {
             System.out.println(e);
+            disableMod.set(true);
         }
     }
 }
